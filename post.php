49fe2c89a26ab7b369514777de9ea066fcb0ccc6
Kana Ando <kanaa@KananoMacBook-Pro.local>
2018-02-06 20:55:36 +0800

    test

--- post.php
+++ post.php
@@ -1,3 +1,45 @@
+<?php 
+session_start();
+require('dbconnect.php');
+
+
+if(isset($_POST) && !empty($_POST)){
+  
+// 投稿をDBに登録
+$sql = "INSERT INTO `packingme_posts` (`user_id`, `pic`, `category_id`, `place`, `term`, `backpack`, `weight`, `detail`, `created`) VALUES (?,?,?,?,?,?,?,?,now());";
+
+// SQL実行
+$data = array($_SESSION["id"],$_POST["pic"],$_POST["category"],$_POST["place"],$_POST["term"],$_POST["backpack"],$_POST["weight"],$_POST["detail"]);
+$stmt = $dbh->prepare($sql);
+$stmt->execute($data);
+
+// 画像の拡張子チェック
+// jpg,png,gifはok
+if(!isset($error)){
+  $ext = substr($_FILES['pic']['name'], -3);
+
+  if(($ext == 'png') || ($ext == 'jpg') || ($ext == 'gif')){
+    // 画像のアップロード処理
+    $pic_name = date('YmdHis') . $_FILES['pic']['name'];
+    // アップロード
+    move_uploaded_file($_FILES['pic']['tmp_name'], 'pic/' . $pic_name);
+
+    // SESSION変数に入力された画像を保存
+    $_SESSION['post'] = $_POST;
+    $_SESSION['post']['pic_name'] = $pic_name;
+
+    header('Location: home.php');
+    exit();
+  }else{
+    $error["image"] = 'type';
+  }
+}
+
+}
+
+ ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -60,21 +102,19 @@
       </div>
     </nav>
 <!-- ヘッダーここまで -->
-    
-    <div>
-      <div class="top-top-top">
-        <img class="post-post-img" src="packing.png" >
-      </div>
-    </div>
-      <div class="post-form">
-        <form action="home.html">
-          <input type="file" name="picture_path" class="form-control">
+    <div class="post-form">
+      <form method="POST" action="" role="form" enctype="multipart/form-data">
+        <!-- 投稿写真 -->
+        <div class="top-top-top">
+        <input type="file" name="pic" class="form-control"></div>
+        <div class="preview">
+        </div>
           <select name="categories"> 
             <option value="1" selected="">タイプを選択</option> 
             <option value="2">Traveler</option> 
             <option value="3" >Engineer</option>  
           </select>
-          <select name="categories"> 
+          <select name="category"> 
             <option value="1" selected="">カテゴリを選択</option> 
             <option value="2">3日以内</option> 
             <option value="3" >2週間以内</option> 
@@ -83,16 +123,17 @@
             <option value="4">1ヶ月以上</option> 
           </select>
           <center>場所</center>
-          <input type="" name="" placeholder="例）フィリピン　セブ島">
+          <input type="" name="place" placeholder="例）フィリピン　セブ島">
           <center>期間</center>
-          <input type="" name="" placeholder="例）４日間">
+          <input type="" name="term" placeholder="例）４日間">
           <center>backpack</center>
-          <input type="" name="" placeholder="例）the north face Caelus 35L">
+          <input type="" name="backpack" placeholder="例）the north face Caelus 35L">
           <center>重量</center>
-          <input type="" name="" placeholder="例）13kg">
+          <input type="" name="weight" placeholder="例）13kg">
           <center>中身詳細</center>
-          <textarea placeholder="例）mackbookpro, dji spark, omd-em5 mark2, t-shirts 3, pants 3..."></textarea>
-          <a href="home.html"><button>投稿する</button></a>
+          <textarea name="detail" placeholder="例）mackbookpro, dji spark, omd-em5 mark2, t-shirts 3, pants 3..."></textarea>
+          <br>
+          <input type="submit" value="編集する" class="btn btn-xl btn-primary">
         </form>
        </div> 
 
@@ -133,6 +174,7 @@
     <!-- Custom scripts for this template -->
     <script src="js/agency.min.js"></script>
     <script src="packing_me.js"></script>
+    <script src="js/post_pic.js"></script>
   </body>
 
 </html>
