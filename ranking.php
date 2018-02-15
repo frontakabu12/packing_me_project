548cd2e06a65741b981addb4e2271fdbe70eade4
Kana Ando <kanaa@KananoMacBook-Pro.local>
2018-02-15 15:27:24 +0800

    ランキング表示　

--- ranking.php
+++ ranking.php
@@ -1,3 +1,66 @@
+<?php 
+  session_start();
+  require('dbconnect.php');
+
+  // ログインユーザーIDからMembersテーブルとPostテーブルを結合して全件取得するsql
+  $sql = "SELECT `packingme_posts`.*,`packingme_users`.`user_name`,`picture_path`, `packingme_likes`.`post_id` , COUNT(*)as `like_count`
+          FROM`packingme_posts` 
+          INNER JOIN `packingme_users` ON `packingme_posts`.`user_id`=`packingme_users`.`id`
+          INNER JOIN `packingme_likes` ON `packingme_posts`.`post_id`=`packingme_likes`.`post_id`  
+          GROUP BY `packingme_likes`.`post_id`
+          ORDER BY `like_count` DESC LIMIT 9";
+  // 実行
+  $stmt = $dbh->prepare($sql);
+  $stmt->execute();
+  // フェッチ
+  $post_list = array();
+  
+  
+
+
+  while(1){
+    // $for_login_user = $stmt->fetch(PDO::FETCH_ASSOC);
+    // $one_post = $stmt->fetch(PDO::FETCH_ASSOC);
+    if($for_login_user == false){
+      break;
+    }else{
+    // LIKE数を求めるSQL文作成
+      // $like_sql = "SELECT COUNT(*)as`like_count` FROM `packingme_likes` WHERE `post_id`=".$for_login_user["post_id"];
+
+      // $like_sql = "SELECT `post_id`, COUNT(*) AS `like_count` FROM `packingme_likes` GROUP BY `post_id` ORDER BY `like_count` DESC";
+
+      // // Sql実行
+      // $like_stmt = $dbh->prepare($like_sql);
+      // $like_stmt->execute();
+
+      // $like_number = $like_stmt->fetch(PDO::FETCH_ASSOC);
+
+//一行ぶんのデータに新しいキーを用意してLIKE数を代入 
+      // $for_login_user["like_count"] = $like_number["like_count"];
+
+//ログインしている人がLIKEしているかどうかの情報を取得
+      $login_like_sql = "SELECT COUNT(*)as`like_flag` FROM `packingme_likes` WHERE `post_id`=".$for_login_user["post_id"]." AND `user_id`=".$_SESSION["id"]; 
+
+// SQL実行
+      $login_like_stmt = $dbh->prepare($login_like_sql);
+      $login_like_stmt->execute();
+
+// フェッチして取得
+      $login_like_number = $login_like_stmt->fetch(PDO::FETCH_ASSOC);
+
+      // echo "<pre>";
+      // var_dump($login_like_number['like_flag']);exit;
+      // echo "</pre>";
+
+      $for_login_user["login_like_flag"] = $login_like_number["like_flag"];
+      // データ取得できている      $tweet_list[] = $one_tweet;
+      $post_list[] = $for_login_user;
+    }
+  }
+
+ ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -38,7 +101,7 @@
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav text-uppercase ml-auto">
             <div class="crown-icon">
-              <a href="ranking.html">
+              <a href="ranking.php">
                 <img src="img/portfolio/crown.png">
               </a>
             </div>
@@ -46,7 +109,7 @@
               <a class="nav-link js-scroll-trigger" href="home.php">Home</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link js-scroll-trigger" href="mypage.php">My page</a>
+              <a class="nav-link js-scroll-trigger" href="mypage.php?user_id=<?php echo $_SESSION["id"]; ?>">My page</a>
             </li>
             <li class="nav-item">
               <a class="nav-link js-scroll-trigger" href="post.php">投稿する</a>
@@ -70,153 +133,116 @@
           </div>
         </div>
         <div class="row">
+          <!-- 1位表示 -->
+          <?php for ($i=1; $i <2 ; $i++) { 
+            $one_post = $stmt->fetch(PDO::FETCH_ASSOC); ?>
           <div class="col-lg-12  portfolio-item">
+            
             <div class="big-crown">
             <img src="img/portfolio/crown.png" width="100" height="100" ><p>No.1</p></div>
-            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
+            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal<?php echo $one_post["post_id"];?>">
               <div class="portfolio-hover">
                 <div class="portfolio-hover-content">
                   <i class="fa fa-plus fa-3x"></i>
                 </div>
               </div>
-              <img class="img-fluid" src="zaki.png" width="600" height="400" alt="">
+              <img class="img-fluid" src="pic/<?php echo $one_post["pic"]; ?>" width="600" height="400" alt="">
             </a>
             <div class="portfolio-caption">
-              <i class="fa fa-suitcase fa-2x"> 200,000 like</i>
-              <!-- <p class="text-more">more</p> -->
+              <!-- いいね部分 -->
+              <?php if($one_post["login_like_flag"] == 0){ ?>
+              <a href="like_buttton.php?like_post_id=<?php echo $one_post["post_id"] ;?>"><i class="fa fa-suitcase fa-2x" style="color:#d4cfc0; "></i></a><span style="font-size:2em;line-height:2em;"><?php echo $one_post["like_count"] ;?> like</span>
+              <!-- いいね取り消し部分 -->
+              <?php }else{?>
+              <a class="unlike" href="like_buttton.php?unlike_post_id=<?php echo $one_post["post_id"] ;?>"><i class="fa fa-suitcase fa-2x" ></i></a><span style="font-size:2em;line-height:2em;"><?php echo $one_post["like_count"]; ?> like</span>
+              <?php }?>
+              <!-- ここまでいいねいいね取り消し部分 -->
             </div>
           </div>
+          <?php } ?>
 
-          <div class="col-lg-12 col-sm-6 portfolio-item">
+          <!-- 2位表示 -->
+          <?php for ($i=2; $i <3 ; $i++) { 
+            $one_post = $stmt->fetch(PDO::FETCH_ASSOC); ?>
+          <div class="col-lg-12  portfolio-item">
+            
             <div class="big-crown ranking_top">
             <img src="img/portfolio/icon-crown.png" width="100" height="100" ><p>No.2</p></div>
-            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal2">
+            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal<?php echo $one_post["post_id"];?>">
               <div class="portfolio-hover">
                 <div class="portfolio-hover-content">
                   <i class="fa fa-plus fa-3x"></i>
                 </div>
               </div>
-              <img class="img-fluid" src="nao.png" width="600" height="400" alt="">
+              <img class="img-fluid" src="pic/<?php echo $one_post["pic"]; ?>" width="600" height="400" alt="">
             </a>
             <div class="portfolio-caption">
-              <i class="fa fa-suitcase fa-2x"> 1000 like</i>
-              <!-- <p class="text-more">more</p> -->
+              <!-- いいね部分 -->
+              <?php if($one_post["login_like_flag"] == 0){ ?>
+              <a href="like_buttton.php?like_post_id=<?php echo $one_post["post_id"] ;?>"><i class="fa fa-suitcase fa-2x" style="color:#d4cfc0; "></i></a><span style="font-size:2em;line-height:2em;"><?php echo $one_post["like_count"] ;?> like</span>
+              <!-- いいね取り消し部分 -->
+              <?php }else{?>
+              <a class="unlike" href="like_buttton.php?unlike_post_id=<?php echo $one_post["post_id"] ;?>"><i class="fa fa-suitcase fa-2x" ></i></a><span style="font-size:2em;line-height:2em;"><?php echo $one_post["like_count"]; ?> like</span>
+              <?php }?>
+              <!-- ここまでいいねいいね取り消し部分 -->
             </div>
           </div>
+          <?php } ?>
 
-          <div class="col-lg-12 col-sm-6 portfolio-item">
+          <!-- 3位表示 -->
+          <?php for ($i=3; $i <4 ; $i++) { 
+            $one_post = $stmt->fetch(PDO::FETCH_ASSOC); ?>
+          <div class="col-lg-12  portfolio-item">
             <div class="big-crown ranking_top">
             <img src="img/portfolio/icon-crown.png" width="100" height="100" ><p>No.3</p></div>
-            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal3">
+            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal<?php echo $one_post["post_id"];?>">
               <div class="portfolio-hover">
                 <div class="portfolio-hover-content">
                   <i class="fa fa-plus fa-3x"></i>
                 </div>
               </div>
-              <img class="img-fluid" src="packing.png" width="600" height="400" alt="">
+              <img class="img-fluid" src="pic/<?php echo $one_post["pic"]; ?>" width="600" height="400" alt="">
             </a>
             <div class="portfolio-caption">
-              <i class="fa fa-suitcase fa-2x"> 100 like</i>
-              <!-- <p class="text-more">more</p> -->
+              <!-- いいね部分 -->
+              <?php if($one_post["login_like_flag"] == 0){ ?>
+              <a href="like_buttton.php?like_post_id=<?php echo $one_post["post_id"] ;?>"><i class="fa fa-suitcase fa-2x" style="color:#d4cfc0; "></i></a><span style="font-size:2em;line-height:2em;"><?php echo $one_post["like_count"] ;?> like</span>
+              <!-- いいね取り消し部分 -->
+              <?php }else{?>
+              <a class="unlike" href="like_buttton.php?unlike_post_id=<?php echo $one_post["post_id"] ;?>"><i class="fa fa-suitcase fa-2x" ></i></a><span style="font-size:2em;line-height:2em;"><?php echo $one_post["like_count"]; ?> like</span>
+              <?php }?>
+              <!-- ここまでいいねいいね取り消し部分 -->
             </div>
           </div>
-          <div class="col-md-4 col-sm-6 portfolio-item">
-            <div class="small-crown">
-            <img src="img/portfolio/small-crown.png" width="60" height="60" ><p>No.4</p></div>
-            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal4">
-              <div class="portfolio-hover">
-                <div class="portfolio-hover-content">
-                  <i class="fa fa-plus fa-3x"></i>
-                </div>
-              </div>
-              <img class="img-fluid" src="inside2.png" alt="">
-            </a>
-            <div class="portfolio-caption">
-              <i class="fa fa-suitcase fa-2x"> 67 like</i>
-              <!-- <p class="text-more">more</p> -->
-            </div>
-          </div>
-          <div class="col-md-4 col-sm-6 portfolio-item">
-            <div class="small-crown">
-            <img src="img/portfolio/small-crown.png" width="60" height="60" ><p>No.5</p></div>
-            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal5">
-              <div class="portfolio-hover">
-                <div class="portfolio-hover-content">
-                  <i class="fa fa-plus fa-3x"></i>
-                </div>
-              </div>
-              <img class="img-fluid" src="naonaonao.png" alt="">
-            </a>
-            <div class="portfolio-caption">
-              <i class="fa fa-suitcase fa-2x"> 50 like</i>
-              <!-- <p class="text-more">more</p> -->
-            </div>
-          </div>
+          <?php } ?>
 
-          <div class="col-md-4 col-sm-6 portfolio-item">
+          <!-- 4位〜9位表示 -->
+          <?php for ($i=4; $i <10 ; $i++) { 
+            $one_post = $stmt->fetch(PDO::FETCH_ASSOC); ?>
+          <div class="col-md-4 col-sm-6  portfolio-item">
             <div class="small-crown">
-            <img src="img/portfolio/small-crown.png" width="60" height="60" ><p>No.6</p></div>
-            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal6">
+            <img src="img/portfolio/small-crown.png" width="60" height="60" ><p>No.<?php echo $i; ?></p></div>
+            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal<?php echo $one_post["post_id"];?>">
               <div class="portfolio-hover">
                 <div class="portfolio-hover-content">
                   <i class="fa fa-plus fa-3x"></i>
                 </div>
               </div>
-              <img class="img-fluid" src="inside3.png" alt="">
+              <img class="img-fluid" src="pic/<?php echo $one_post["pic"]; ?>" width="350" height="300" alt="">
             </a>
             <div class="portfolio-caption">
-              <i class="fa fa-suitcase fa-2x"> 33 like</i>
-              <!-- <p class="text-more">more</p> -->
+              <!-- いいね部分 -->
+              <?php if($one_post["login_like_flag"] == 0){ ?>
+              <a href="like_buttton.php?like_post_id=<?php echo $one_post["post_id"] ;?>"><i class="fa fa-suitcase fa-2x" style="color:#d4cfc0; "></i></a><span style="font-size:2em;line-height:2em;"><?php echo $one_post["like_count"] ;?> like</span>
+              <!-- いいね取り消し部分 -->
+              <?php }else{?>
+              <a class="unlike" href="like_buttton.php?unlike_post_id=<?php echo $one_post["post_id"] ;?>"><i class="fa fa-suitcase fa-2x" ></i></a><span style="font-size:2em;line-height:2em;"><?php echo $one_post["like_count"]; ?> like</span>
+              <?php }?>
+              <!-- ここまでいいねいいね取り消し部分 -->
             </div>
           </div>
-          <div class="col-md-4 col-sm-6 portfolio-item">
-            <div class="small-crown">
-            <img src="img/portfolio/small-crown.png" width="60" height="60" ><p>No.7</p></div>
-            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal4">
-              <div class="portfolio-hover">
-                <div class="portfolio-hover-content">
-                  <i class="fa fa-plus fa-3x"></i>
-                </div>
-              </div>
-              <img class="img-fluid" src="inside4.png" alt="">
-            </a>
-            <div class="portfolio-caption">
-              <i class="fa fa-suitcase fa-2x"> 27 like</i>
-              <!-- <p class="text-more">more</p> -->
-            </div>
-          </div>
-          <div class="col-md-4 col-sm-6 portfolio-item">
-            <div class="small-crown">
-            <img src="img/portfolio/small-crown.png" width="60" height="60" ><p>No.8</p></div>
-            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal5">
-              <div class="portfolio-hover">
-                <div class="portfolio-hover-content">
-                  <i class="fa fa-plus fa-3x"></i>
-                </div>
-              </div>
-              <img class="img-fluid" src="inside5.jpg" alt="">
-            </a>
-            <div class="portfolio-caption">
-              <i class="fa fa-suitcase fa-2x"> 20 like</i>
-              <!-- <p class="text-more">more</p> -->
-            </div>
-          </div>
-          <div class="col-md-4 col-sm-6 portfolio-item">
-            <div class="small-crown">
-            <img src="img/portfolio/small-crown.png" width="60" height="60" ><p>No.9</p></div>
-            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal6">
-              <div class="portfolio-hover">
-                <div class="portfolio-hover-content">
-                  <i class="fa fa-plus fa-3x"></i>
-                </div>
-              </div>
-              <img class="img-fluid" src="inside1.png" alt="">
-            </a>
-            <div class="portfolio-caption">
-              <i class="fa fa-suitcase fa-2x"> 11 like</i>
-              <!-- <p class="text-more">more</p> -->
-            </div>
-          </div>
+          <?php } ?>
+          <!-- 繰り返し終了 -->
         </div>
       </div>
     </section>
@@ -262,7 +288,7 @@
     <!-- Portfolio Modals -->
 
     <!-- Modal 1 -->
-      <div class="portfolio-modal modal fade" id="portfolioModal1" tabindex="-1" role="dialog" aria-hidden="true">
+      <div class="portfolio-modal modal fade" id="portfolioModal<?php echo $one_post["post_id"];?>" tabindex="-1" role="dialog" aria-hidden="true">
       <div class="modal-dialog">
         <div class="modal-content">
           <div class="close-modal" data-dismiss="modal">
